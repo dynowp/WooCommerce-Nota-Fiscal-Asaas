696a153f82a1513c481d0554746cedaec0c101b6
Diogo Macedo <dmacedo3500@gmail.com>
2024-10-20 00:42:01 -0300

    Version 1.0.1

--- woo-nota-fiscal-for-assas.php
+++ woo-nota-fiscal-for-assas.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Nota Fiscal for Asaas
  * Description: Integração do WooCommerce com o Asaas para emissão automática de Nota Fiscal.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: DynoWP
